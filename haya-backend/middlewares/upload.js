3805f6fa9eff43a237983ea96818bf9c5f167273
biggeraudience <130661217+biggeraudience@users.noreply.github.com>
2025-05-06 22:26:09 +0100

    Update upload.js

--- haya-backend/middlewares/upload.js
+++ haya-backend/middlewares/upload.js
@@ -1,9 +1,10 @@
-import multer from 'multer';
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-
-// Export a function that takes the Worker `env`
+// ~/haya-backend/middlewares/upload.js
 export default (env) => {
+  const multer  = require('multer');
+  const cloudinaryModule = require('cloudinary');
+  const cloudinary = cloudinaryModule.v2;
+  const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
   cloudinary.config({
     cloud_name:  env.CLOUDINARY_CLOUD_NAME,
     api_key:     env.CLOUDINARY_API_KEY,
@@ -19,6 +20,5 @@ export default (env) => {
     },
   });
 
-  const upload = multer({ storage });
-  return upload;
+  return multer({ storage });
 };
