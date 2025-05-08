e9b11c8703dab99645781ebbfc6dc314d1c2e4ad
biggeraudience <130661217+biggeraudience@users.noreply.github.com>
2025-05-08 19:17:19 +0100

    Update worker.js

--- api/worker.js
+++ api/worker.js
@@ -5,7 +5,22 @@ import createApp from "../haya-backend/server.js";
 
 let handler; 
 
+export default {// ~/api/worker.js
+import serverless from "serverless-http";
+import createApp from "../haya-backend/server.js";
+
+let handler;
+
 export default {
+  async fetch(request, env, ctx) {
+    if (!handler) {
+      const app = createApp(env);
+      handler = serverless(app);
+    }
+    return handler(request, { event: request, context: ctx });
+  },
+};
+
     async fetch(request, env, ctx) {
        
         if (!handler) {
