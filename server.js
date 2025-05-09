f5716460b0e11d22e51ce9c3dbb155147dc093c7
biggeraudience <130661217+biggeraudience@users.noreply.github.com>
2025-05-09 11:05:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 // api/server.js
 import createApp from "../haya-backend/server.js";
-
 let app; 
-
 export default async function handler(req, res) {
-
   if (!app) {
     app = await createApp(process.env);
   }
-
   return app(req, res);
 }
