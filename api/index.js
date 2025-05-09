93eea3ece88016c3b91250132e15a0ed230c9adc
biggeraudience <130661217+biggeraudience@users.noreply.github.com>
2025-05-09 11:21:56 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 // api/index.js
-import serverless from "serverless-http";      
+import serverless from "serverless-http";
 import createApp   from "../haya-backend/server.js";
 
 let handler;
-
 export default async function (req, res) {
   if (!handler) {
-    const app = await createApp(process.env);
-    handler  = serverless(app);
+    const app     = await createApp(process.env);
+    handler      = serverless(app);
   }
   return handler(req, res);
 }
