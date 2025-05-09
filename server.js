5fd30d0d1db4bb0ced39d541114af2f89aa9dd8b
biggeraudience <130661217+biggeraudience@users.noreply.github.com>
2025-05-09 11:21:11 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import createApp from "../haya-backend/server.js";
-let app; 
-export default async function handler(req, res) {
-  if (!app) {
-    app = await createApp(process.env);
-  }
-  return app(req, res);
-}
